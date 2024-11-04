ecb56ce29e19f6c83cb10269ec36b64aa4e206f0
Guizin-programador <guigama453@gmail.com>
2024-11-04 17:25:44 -0300

    adicionando player
    [feat] cena e script pro player

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_FORCE = -400.0
 var is_jumping = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
